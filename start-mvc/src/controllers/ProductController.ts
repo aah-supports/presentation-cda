2c4cccff10a214370d841e5fceeb870f1e93b18c
Antoine07 <antoine.lucsko@wanadoo.fr>
2026-04-17 13:18:28 +0200

    feat(product-controller): add exception handler method

--- start-mvc/src/controllers/ProductController.ts
+++ start-mvc/src/controllers/ProductController.ts
@@ -2,8 +2,34 @@ import { Request, Response } from "express";
 
 export class ProductController {
 
-  async list (_req: Request, res: Response): Promise<void> {
+  constructor(service){}
 
-    res.json({ data: "products" });
-  };
+  async all(_req: Request, res: Response): Promise<void> {
+   
+  }
+
+  async getById(req: Request, res: Response): Promise<void> {
+      const { id } = req.body as { id?: string };
+
+      if(!id){
+        return res.status(400).json({ error: "PRODUCT_ID_REQUIRED" });
+      }
+
+      const product = await this.service.getProduct(productId);
+
+      res.json({ data: "products" });
+  }
+
+  private async handler(res: Response, action: () => Promise<void>): Promise<void> {
+    try {
+      await action();
+    } catch (error) {
+      console.error("ProductController error:", error);
+
+      if (!res.headersSent) {
+        res.status(500).json({ error: "Internal Server Error" });
+      }
+    }
+  }
+
 }
